71f46fbd179dcf92b92a36c1044623a044c5a65b
Nick Everitt <nicholaseveritt@gmail.com>
2023-12-03 11:02:04 +0000

    Improve gear ratios

--- solutions/03_gear_ratios.hs
+++ solutions/03_gear_ratios.hs
@@ -5,78 +5,47 @@ import System.Environment (getArgs)
 main = do
     args <- getArgs
     input <- readFile (args !! 0)
-    let answer = gearRatios (lines input)
-    print answer
-
-gearRatios :: [String] -> Int
-gearRatios schematic = answer2 schematic
+    let schematic = lines input
+    print (answer1 schematic)
+    print (answer2 schematic)
 
 -- Part 1.
 
-answer1 :: [String] -> Int
-answer1 schematic = foldl (+) 0 (getAdjacentNumbers schematic)
-
-getAdjacentNumbers :: [String] -> [Int]
-getAdjacentNumbers strs = map (\(s,n) -> read s :: Int) adjacents
+-- Find the coordinates of all the numbers and symbols. Determine the
+-- list of interior coordinates for each number, and determine the
+-- neighbourhood of coordinates bordering the interior. Qualifying
+-- coordinates are for those numbers with a neighbourhood that contains
+-- a symbol. Extract the number at each qualifying coordinate, and sum.
+answer1 schematic =
+    foldl (+) 0 (map (numberAt schematic) qualifyingCoords)
     where
-        nums = getNumbers strs [] 0
-        hasSymbol str = (find isSymbol str) /= Nothing
-        adjacents = filter (\(s,n) -> hasSymbol n) nums
-
-getNumbers :: [[Char]] -> [(String, String)] -> Int -> [(String, String)]
-getNumbers strs nums y =
-    if y == length strs then nums
-    else getNumbers strs (nums ++ getNumbersLine strs [] 0 y) (y + 1)
-
-getNumbersLine :: [[Char]] -> [(String, String)] -> Int -> Int -> [(String, String)]
-getNumbersLine strs nums x y =
-    if x == length str 
-        then filtered nums
-        else if isDigit ch
-            then getNumbersLine strs (addNeighboursToFirst (addCharToFirst nums ch) neighs) (x + 1) y
-            else getNumbersLine strs (([],[]) : nums) (x + 1) y
-    where
-        str = strs !! y
-        ch = str !! x
-        neighs = neighbours' strs x y [] neighbourOffsets
-        filtered nums = filter (\(s,n) -> s /= "") nums
-
-addCharToFirst :: [(String, String)] -> Char -> [(String, String)]
-addCharToFirst [] ch = [([ch], [])]
-addCharToFirst ((hs, hn) : remainder) ch = (hs ++ [ch], hn) : remainder
-
-addNeighboursToFirst :: [(String, String)] -> [Char] -> [(String, String)]
-addNeighboursToFirst [] chs = [([], chs)]
-addNeighboursToFirst ((hs, hn) : remainder) ch = (hs, hn ++ ch) : remainder
-
-at :: [String] -> Int -> Int -> Maybe Char
-at strs x y = if isLegal then Just ((strs !! y) !! x) else Nothing
-    where
-        width = length (strs !! 0)
-        height = length strs
-        isLegal = x >= 0 && x < width && y >= 0 && y < height
-
-neighbourOffsets = [(-1,1), (0,1), (1,1), (-1,0), (1,0), (-1,-1), (0,-1), (1,-1)]
-
-neighbours' :: [String] -> Int -> Int -> [Char] -> [(Int, Int)] -> [Char]
-neighbours' strs x y neighs [] = neighs
-neighbours' strs x y neighs ((xoff, yoff) : offsets) = case at strs (x + xoff) (y + yoff) of
-    Just ch -> neighbours' strs x y (ch : neighs) offsets
-    Nothing -> neighbours' strs x y neighs offsets
+        symbols = symbolCoords schematic
+        numbers = numberCoords schematic
+        (w, h) = size schematic
+        len (nx, ny) = numberLength schematic (nx, ny)
+        interior (nx, ny) = [(i, ny) | i <- [nx..nx - 1 + len (nx, ny)]]
+        neighbourhood (nx, ny) = [c | c <- concatMap (neighbours (w, h)) (interior (nx, ny))]
+        at (x, y) = (schematic !! y) !! x
+        containsSymbol coords = (find (isSymbol . at) coords) /= Nothing
+        qualifyingCoords = filter (containsSymbol . neighbourhood) numbers
 
 -- Part 2.
 
+-- Find the coordinates of all the gears. For each, find the coordinates
+-- of adjacent numbers and extract those numbers from the schematic.
+-- Compute the product when there are two adjacent numbers, and sum.
 answer2 schematic =
-    foldl (+) 0 (map (ratio schematic) gears)
+    foldl (+) 0 (map (ratio . extracts) gears)
     where
+        nums = numberCoords schematic
         gears = gearCoords schematic
+        adjacents gear = adjacentNumbers schematic nums gear
+        extracts gear = map (numberAt schematic) (adjacents gear)
+        ratio acts = if length acts /= 2 then 0 else foldl (*) 1 acts
 
-ratio schematic gear =
-    if length adjs /= 2 then 0 else (nums !! 0) * (nums !! 1)
-    where
-        adjs = adjNumbers schematic gear
-        nums = map (numberAt schematic) adjs
+-- Schematic parsing.
 
+-- Returns an Int representation of the number at coordinate (x, y).
 numberAt schematic (x, y) =
     read str :: Int
     where
@@ -84,35 +53,41 @@ numberAt schematic (x, y) =
         row = schematic !! y
         str = take len (drop x row)
 
-adjNumbers schematic (gx, gy) =
-    filter (areAnyInsideNumber schematic ns) numbers 
+-- Returns a list containing the coordinates of any number that extends
+-- into a square bordering the coordinate (x, y).
+adjacentNumbers schematic nums (x, y) =
+    filter (areAnyInsideNumber schematic ns) nums 
     where
-        numbers = numberCoords schematic
-        ns = neighbours schematic (gx, gy)
+        (w, h) = size schematic
+        ns = neighbours (w, h) (x, y)
 
+-- Returns a list of the coordinates of all gears.
 gearCoords schematic = filter (isGear . at) (allCoords schematic) 
     where
         at (x, y) = (schematic !! y) !! x
-        isGear ch = ch == '*'
 
-digitCoords schematic = filter (isDigit . at) (allCoords schematic)
+-- Returns a list of the coordinates of the first digit of all numbers.
+numberCoords schematic = filter isFirst coords
+    where
+        at (x, y) = (schematic !! y) !! x
+        coords = filter (isDigit . at) (allCoords schematic)
+        isFirst (x, y) = (find (\(i,j) -> j == y && i == x-1) coords) == Nothing
+
+-- Returns a list of the coordinates of all symbols.
+symbolCoords schematic = filter (isSymbol . at) (allCoords schematic)
     where
         at (x, y) = (schematic !! y) !! x
 
-numberCoords schematic =
-    filter isFirst coords
-    where
-        coords = digitCoords schematic
-        isFirst (x, y) = (find (\(i,j) -> j == y && i == x-1) coords) == Nothing
-
-isInsideNumber schematic (nx, ny) (x, y) =
-    ny == y && x >= nx && x <= xLast
-    where
-        xLast = nx - 1 + numberLength schematic (nx, ny)
-
+-- Returns true if any coordinate from the coords list is inside the
+-- extent of the number beginning at coordinate (nx, ny), otherwise false.
 areAnyInsideNumber schematic coords (nx, ny) =
-    find (isInsideNumber schematic (nx, ny)) coords /= Nothing
+    find (isInside) coords /= Nothing
+    where
+        len = numberLength schematic (nx, ny)
+        isInside (x, y) = ny == y && x >= nx && x < nx + len
 
+-- Returns a count of the number of digits in the number beginning at
+-- coordinate (x, y).
 numberLength schematic (x, y) =
     if x < width - 1 && this > 0 then this + next else this
     where
@@ -120,27 +95,27 @@ numberLength schematic (x, y) =
         next = numberLength schematic (x + 1, y)
         width = length (schematic !! 0)
 
-neighbours schematic (x, y) =
+-- Given a schematic size of (w, h), returns a list of all coordinates
+-- that border coordinate (x, y) and are inside the schematic's bounds.
+neighbours (w, h) (x, y) =
     filter isLegal candidates
     where
-        candidates = map (\(ox,oy) -> (x + ox, y + oy)) neighbourOffsets
-        width = length (schematic !! 0)
-        height = length schematic
-        isLegal (x, y) = x >= 0 && x < width && y >= 0 && y <= height
+        offsets = [(-1,1),(0,1),(1,1),(-1,0),(1,0),(-1,-1),(0,-1),(1,-1)]
+        candidates = map (\(ox,oy) -> (x + ox, y + oy)) offsets
+        isLegal (x, y) = x >= 0 && x < w && y >= 0 && y < h
 
+-- Returns (w, h), the width and height of the schematic.
+size schematic = (length (schematic !! 0), length schematic)
+
+-- Returns a list of all coordinates within the schematic.
 allCoords schematic =
-    [(x,y) | x <- [0..width-1], y <- [0..height-1]]
-    where
-        width = length (schematic !! 0)
-        height = length schematic
+    [(x, y) | x <- [0..w-1], y <- [0..h-1]] where (w, h) = size schematic
 
-
-        
-
--- Schematic parsing.
-
-isDigit :: Char -> Bool
+-- True if ch is a digit, false otherwise.
 isDigit ch = ord ch >= ord('0') && ord ch <= ord('9')
 
-isSymbol :: Char -> Bool
-isSymbol ch = ch /= '.' && not (isDigit ch) 
+-- True if ch is a symbol, false otherwise.
+isSymbol ch = ch /= '.' && not (isDigit ch)
+
+-- True if ch is a gear, false otherwise.
+isGear ch = ch == '*'
